26f65fbb1625a3b2f5bc83c62ca3754a0f7cd4b5
Tushar Kathuria <tushar.kathuria@compass.com>
2021-03-03 15:23:30 +0530

    Added endpoint for image filter

--- src/server.ts
+++ src/server.ts
@@ -14,29 +14,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.use(bodyParser.json());
 
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async(req, res) => {
+    let { image_url } = req.query
 
-  app.get("/filteredimage?image_url={{url}}", async(req, res) => {
-    let { url } = req.params
-
-    if(!url) {
+    if(!image_url) {
       return res.status(400).send({ message: 'Image URL is missing or malformed' });
     }
 
-    let outputPath = await filterImageFromURL(url)
+    let outputPath = await filterImageFromURL(image_url)
 
-    res.sendFile(outputPath)
-    deleteLocalFiles([outputPath])
+    res.sendFile(outputPath, () => {
+      deleteLocalFiles([outputPath])
+    })
   })
 
   //! END @TODO1
