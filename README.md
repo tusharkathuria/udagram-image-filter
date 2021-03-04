d8ed2c888f7aa3b97292777225d19b4335f1c072
Tushar Kathuria <tushar.kathuria@compass.com>
2021-03-04 18:26:30 +0530

    Added types and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Udagram Image Filtering Microservice Submission
 
+Github link: https://github.com/tusharkathuria/udagram-image-filter
+
 ## Endpoint link
 http://udagram-image-tushar-filter-dev-dev.ap-south-1.elasticbeanstalk.com/filteredimage?image_url=https://picsum.photos/600
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { Request, Response } from "express";
 
 (async () => {
 
@@ -14,14 +15,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.use(bodyParser.json());
 
   // GET /filteredimage?image_url={{URL}}
-  app.get("/filteredimage", async(req, res) => {
-    let { image_url } = req.query
+  app.get("/filteredimage", async(req: Request, res: Response) => {
+    let image_url: string = req.query.image_url
 
     if(!image_url) {
       return res.status(400).send({ message: 'Image URL is missing or malformed' });
     }
 
-    let outputPath = await filterImageFromURL(image_url)
+    let outputPath: string = await filterImageFromURL(image_url)
 
     res.sendFile(outputPath, () => {
       deleteLocalFiles([outputPath])
@@ -32,7 +33,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
